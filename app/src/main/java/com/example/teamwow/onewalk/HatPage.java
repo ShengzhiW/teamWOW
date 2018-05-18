5ddf0fae5598481255be6828ebeb6155007beb25
KimberlyLi <kimli526@gmail.com>
2018-05-17 20:01:04 -0700

    added gridview with scroll and click with placeholders HatPage

--- app/src/main/java/com/example/teamwow/onewalk/HatPage.java
+++ app/src/main/java/com/example/teamwow/onewalk/HatPage.java
@@ -2,14 +2,37 @@ package com.example.teamwow.onewalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class HatPage extends AppCompatActivity {
+    //add click function
+    GridLayout mainGrid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hat_page);
         Bundle bundle = getIntent().getExtras();
+        mainGrid = (GridLayout)findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
+    }
+
+    public void setSingleEvent(GridLayout singleEvent) {
+        for(int i = 0; i < mainGrid.getChildCount();i++){
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //TODO: replace toast with start new Activity
+                    Toast.makeText(HatPage.this, "Clicked at index" + finalI, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
