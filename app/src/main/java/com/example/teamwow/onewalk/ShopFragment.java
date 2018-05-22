4de9e2301fe970df6e358100307352c333d647d3
Jungyong <jungyong55@gmail.com>
2018-05-22 14:55:38 -0700

    Added placeholders to future testing

--- app/src/main/java/com/example/teamwow/onewalk/ShopFragment.java
+++ app/src/main/java/com/example/teamwow/onewalk/ShopFragment.java
@@ -12,7 +12,33 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShopFragment extends Fragment {
+
+    final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    final String uid = user.getUid();
+
+    private DatabaseReference hatDB = db.getReference("Users").child(uid).child("Inventory")
+            .child("Hat");
+    ArrayList<Integer> hatArray = new ArrayList<Integer>();
+
+    private DatabaseReference bodyDB = db.getReference("Users").child(uid).child("Inventory")
+            .child("Body");
+    ArrayList<Integer> bodyArray = new ArrayList<>();
+
+    private int bodyIndex = 9;
+    private int hatIndex = 4;
 
     @Nullable
     @Override
@@ -20,24 +46,6 @@ public class ShopFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_shop, container, false);
         Button hatButton = (Button)view.findViewById(R.id.hatOpen);
         Button bodyButton = (Button)view.findViewById(R.id.bodyOpen);
-
-        hatButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getActivity(), HatPage.class);
-             //   intent.putExtra("some", "hats");
-                startActivity(intent);
-            }
-        });
-
-        bodyButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getActivity(), BodyPage.class);
-              //  intent.putExtra("some", "hats");
-                startActivity(intent);
-            }
-        });
 
         int [] bodiesDrawables = {
                 R.drawable.cowboy_body,
@@ -68,12 +76,54 @@ public class ShopFragment extends Fragment {
                 R.drawable.duck_hat
         };
 
+        hatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), HatPage.class);
+             //   intent.putExtra("some", "hats");
+                startActivity(intent);
+            }
+        });
+
+        bodyButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), BodyPage.class);
+              //  intent.putExtra("some", "hats");
+                startActivity(intent);
+            }
+        });
+
+        /* Set up inventory array */
+        bodyDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //Toast.makeText(HatPage.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+                // the snapshot provides a generic list holding Objects
+                List array = (List) snapshot.getValue();
+
+                // we then transform those objects into integers for our hat array
+                for(int i = 0; i < array.size(); i++){
+                    //Toast.makeText(HatPage.this, "Value is" + Integer.valueOf(array.get(i).toString()), Toast.LENGTH_SHORT).show();
+                    bodyArray.add(Integer.valueOf(array.get(i).toString()));
+                }
+
+            }
+
+            // onCancelled...
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // DO nothing
+            }
+        });
+
         ImageView imgView = (ImageView) view.findViewById(R.id.imgView) ;
-        Drawable drawable = getResources().getDrawable(bodiesDrawables[9]);
+        Drawable drawable = getResources().getDrawable(bodiesDrawables[bodyIndex]);
         imgView.setImageDrawable(drawable);
 
         ImageView imgViewHat = (ImageView) view.findViewById(R.id.imgViewHat) ;
-        Drawable drawableHat = getResources().getDrawable(hatDrawables[4]);
+        Drawable drawableHat = getResources().getDrawable(hatDrawables[hatIndex]);
         imgViewHat.setImageDrawable(drawableHat);
 
         return view;
