ac17efb4996b3aebf11e669a224a2bb6ebbc2f2a
ShengzW <shengz.wg@gmail.com>
2018-05-28 22:23:42 -0700

    added drawable dirs and spashscreen delay

--- app/src/main/java/com/example/teamwow/onewalk/SplashActivity.java
+++ app/src/main/java/com/example/teamwow/onewalk/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.teamwow.onewalk;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,8 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
-        finish();
+        final SplashActivity splash = this;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(splash, Login.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+
     }
 }
