c357987c1e9699feb68feae452dc2829e1de3609
Alex Zhu <er.and.alex@gmail.com>
2018-05-30 12:38:31 -0700

    Revert "Fixed bug regarding leaderboard rankings"
    This reverts commit e21ced90018fd180e1e9afd2652e40601e888a55.

--- app/src/main/java/com/example/teamwow/onewalk/LeaderboardFragment.java
+++ app/src/main/java/com/example/teamwow/onewalk/LeaderboardFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -147,7 +146,7 @@ public class LeaderboardFragment extends Fragment {
             public void onCancelled(DatabaseError databaseError) {}
         });
 
-        Query leaderQuery = db.getReference("Leaderboard").orderByChild("Steps").limitToLast(10);
+        Query leaderQuery = db.getReference("Leaderboard").orderByChild("Steps");
 
         // attaches a listener to check when a user's step count is updated
         leaderQuery.addValueEventListener(new ValueEventListener() {
@@ -158,7 +157,6 @@ public class LeaderboardFragment extends Fragment {
                 int totalMembers = 0;
                 for (DataSnapshot leaderSnapshot: dataSnapshot.getChildren()) {
                     if(leaderSnapshot.child("Steps").exists()) {
-                        Log.d("User", leaderSnapshot.getKey());
                         if((leaderSnapshot.child("Private").getValue()).equals(false)) {
                             names.add(leaderSnapshot.child("Name").getValue(String.class));
 
