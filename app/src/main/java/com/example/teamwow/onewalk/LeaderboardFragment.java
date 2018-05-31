47d2e19d008849134a21c3ead4b4027748f39d53
Alex Zhu <er.and.alex@gmail.com>
2018-05-30 17:43:38 -0700

    Minor leaderboard bug fixed

--- app/src/main/java/com/example/teamwow/onewalk/LeaderboardFragment.java
+++ app/src/main/java/com/example/teamwow/onewalk/LeaderboardFragment.java
@@ -146,7 +146,7 @@ public class LeaderboardFragment extends Fragment {
             public void onCancelled(DatabaseError databaseError) {}
         });
 
-        Query leaderQuery = db.getReference("Leaderboard").orderByChild("Steps");
+        Query leaderQuery = db.getReference("Leaderboard").orderByChild("Steps").limitToLast(11);
 
         // attaches a listener to check when a user's step count is updated
         leaderQuery.addValueEventListener(new ValueEventListener() {
